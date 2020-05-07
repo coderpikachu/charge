e891a0c6be9cefa5162509314f14ce11c42413cf
shiwenbin <1011214027@qq.com>
2020-05-07 19:32:00 +0800

    logIn page signUp page
    next:
     user:
      log in check
      grant authority
      revoke authority
     search
     web style
     type correct

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your models here.
 def home_view(request,*args,**kwargs):
-	my_context={
-	"my_number":123,
-	"my_list":[1,2,3]
-	}
-	return render(request,"home.html",my_context)
+	return render(request,"home.html",{})
+
+def logIn_view(request,*args,**kwargs):
+	if request.method == "POST":  
+        form = UserForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save()  
+                id=form.clean_data('id')
+                pwd=form.clean_data('pwd')
+                return redirect('<id>/<pwd>/home')  
+            except:  
+                pass  
+    else:  
+        form = UserForm()
+    return render(request,"logIn.html",{'form':form})  
+
+def signUp_view(request,*args,**kwargs):
+    if request.method == "POST":  
+        form = UserForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save()  
+                return redirect('/')  
+            except:  
+                pass  
+    else:  
+        form = UserForm()
+    return render(request,"signUp.html",{'form':form})  
--- trydjangos/urls.py
+++ trydjangos/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin  
 from django.urls import path,include
-from pages.views import home_view
+from pages.views import home_view,logIn_view,signUp_view
 urlpatterns = [  
     path('admin/', admin.site.urls),  
-    path('', home_view),   
-    path('student/',include('student.urls')),
-    path('charge/',include('charge.urls')),
-    path('flat/',include('flat.urls')),
-    path('dormitory/',include('dormitory.urls')),
-    path('user/',include('user.urls'))
+    path('', logIn_view),  
+    path('signUp', signUp_view), 
+    path('<int:id>/<int:pwd>/home', home_view),
+    path('<int:id>/<int:pwd>/student',include('student.urls')),
+    path('<int:id>/<int:pwd>/charge',include('charge.urls')),
+    path('<int:id>/<int:pwd>/flat',include('flat.urls')),
+    path('<int:id>/<int:pwd>/dormitory',include('dormitory.urls')),
+    path('<int:id>/<int:pwd>/user',include('user.urls'))
 ]  
